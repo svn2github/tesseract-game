926ff8e0e9c96c12137a8c25719c1db2398fed00
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2016-01-18 18:29:52 +0000

    misc cleanups
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@2058 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/sound.cpp
+++ src/engine/sound.cpp
@@ -8,8 +8,6 @@
   #include "SDL_mixer.h"
 #endif
 
-#define MAXVOL MIX_MAX_VOLUME
-
 bool nosound = true;
 
 struct soundsample
@@ -136,7 +134,7 @@ stream *musicstream = NULL;
 void setmusicvol(int musicvol)
 {
     if(nosound) return;
-    if(music) Mix_VolumeMusic((musicvol*MAXVOL)/255);
+    if(music) Mix_VolumeMusic((musicvol*MIX_MAX_VOLUME)/255);
 }
 
 void stopmusic()
@@ -218,7 +216,7 @@ void startmusic(char *name, char *cmd)
             musicfile = newstring(file);
             if(cmd[0]) musicdonecmd = newstring(cmd);
             Mix_PlayMusic(music, cmd[0] ? 0 : -1);
-            Mix_VolumeMusic((musicvol*MAXVOL)/255);
+            Mix_VolumeMusic((musicvol*MIX_MAX_VOLUME)/255);
             intret(1);
         }
         else
@@ -494,8 +492,8 @@ bool updatechannel(soundchannel &chan)
             pan = int(255.9f*(0.5f - 0.5f*v.x/v.magnitude2())); // range is from 0 (left) to 255 (right)
         }
     }
-    vol = (vol*MAXVOL*chan.slot->volume)/255/255;
-    vol = min(vol, MAXVOL);
+    vol = (vol*MIX_MAX_VOLUME*chan.slot->volume)/255/255;
+    vol = min(vol, MIX_MAX_VOLUME);
     if(vol == chan.volume && pan == chan.pan) return false;
     chan.volume = vol;
     chan.pan = pan;
@@ -699,7 +697,7 @@ void resetsound()
     if(music && loadmusic(musicfile))
     {
         Mix_PlayMusic(music, musicdonecmd ? 0 : -1);
-        Mix_VolumeMusic((musicvol*MAXVOL)/255);
+        Mix_VolumeMusic((musicvol*MIX_MAX_VOLUME)/255);
     }
     else
     {
