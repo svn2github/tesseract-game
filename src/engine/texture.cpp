e4b48c3e9ce8925f313790d922307928db0d2f98
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2016-02-13 02:45:20 +0000

    backport ati_cubemap_bug workaround as it now seems to have reappeared with Catalyst on GCN
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@2096 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -2641,6 +2641,36 @@ Texture *Slot::loadthumbnail()
 
 // environment mapped reflections
 
+void forcecubemapload(GLuint tex)
+{
+    extern int amd_cubemap_bug;
+    if(!amd_cubemap_bug || !tex) return;
+
+    pushhudmatrix();
+    hudmatrix.identity();
+    flushhudmatrix(false);
+    SETSHADER(hudcubemap);
+    GLenum depthtest = glIsEnabled(GL_DEPTH_TEST), blend = glIsEnabled(GL_BLEND);
+    if(depthtest) glDisable(GL_DEPTH_TEST);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, tex);
+    if(!blend) glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    gle::defvertex(2);
+    gle::deftexcoord0(3);
+    gle::defcolor(4);
+    gle::begin(GL_LINES);
+    loopi(2)
+    {
+        gle::attribf(i*1e-3f, 0);
+        gle::attribf(0, 0, 1);
+        gle::attribf(1, 1, 1, 0);
+    }
+    gle::end();
+    if(!blend) glDisable(GL_BLEND);
+    if(depthtest) glEnable(GL_DEPTH_TEST);
+    pophudmatrix(false);
+}
+
 extern const cubemapside cubemapsides[6] =
 {
     { GL_TEXTURE_CUBE_MAP_NEGATIVE_X, "lf", false, true,  true  },
@@ -2760,6 +2790,7 @@ Texture *cubemaploadwildcard(Texture *t, const char *name, bool mipit, bool msg,
             createtexture(!i ? t->id : 0, t->w, t->h, s.data, 3, mipit ? 2 : 1, component, side.target, s.w, s.h, s.pitch, false, format, true);
         }
     }
+    forcecubemapload(t->id);
     return t;
 }
 
@@ -2913,6 +2944,7 @@ GLuint genenvmap(const vec &o, int envmapsize, int blur, bool onlysky)
     glBindFramebuffer_(GL_FRAMEBUFFER, 0);
     glViewport(0, 0, hudw, hudh);
     clientkeepalive();
+    forcecubemapload(tex);
     return tex;
 }
 
