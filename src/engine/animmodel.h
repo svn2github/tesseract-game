eab930bbb6108337bbc67fc75ca3d47893af6e32
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2017-10-26 03:56:42 +0000

    fix translation of linked models
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@2150 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/animmodel.h
+++ src/engine/animmodel.h
@@ -895,7 +895,7 @@ struct animmodel : model
                 matrixstack[matrixpos] = matrixstack[matrixpos-1];
                 matrixstack[matrixpos].rotate(pitchamount*RAD, oaxis);
             }
-            if(!index && !model->translate.iszero())
+            if(this == model->parts[0] && !model->translate.iszero())
             {
                 if(oldpos == matrixpos)
                 {
@@ -981,7 +981,7 @@ struct animmodel : model
                 matrixstack[matrixpos] = matrixstack[matrixpos-1];
                 matrixstack[matrixpos].rotate(pitchamount*RAD, oaxis);
             }
-            if(!index && !model->translate.iszero())
+            if(this == model->parts[0] && !model->translate.iszero())
             {
                 if(oldpos == matrixpos)
                 {
